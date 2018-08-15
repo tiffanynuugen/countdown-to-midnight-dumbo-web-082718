d1deec8a85151ae9cebe64f2b4107ad2ce62625a
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 01:43:07 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,8 +5,9 @@ def countdown(seconds)
     puts "#{seconds} SECOND(S)!"
     seconds -= 1
  end
- puts "HAPPY NEW YEAR!"
+ return "HAPPY NEW YEAR!"
 end
+
 
 def countdown_with_sleep(seconds)
   while seconds > 0
@@ -14,5 +15,5 @@ def countdown_with_sleep(seconds)
     seconds -= 1
     sleep(1)
   end
-  puts "HAPPY NEW YEAR!"
+  return "HAPPY NEW YEAR!"
 end
